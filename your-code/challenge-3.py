334838d16827e1cb5844c8c66689123e5ada1ac7
Alieldin Ramadan <alieldin91@gmail.com>
2019-11-06 10:39:58 +0100

    After finishing all the three challenges

--- your-code/challenge-3.py
+++ your-code/challenge-3.py
@@ -1,28 +1,8 @@
-"""
-You are presented with an integer number larger than 5. Your goal is to identify the longest side
-possible in a right triangle whose sides are not longer than the number you are given.
-
-For example, if you are given the number 15, there are 3 possibilities to compose right triangles:
-
-1. [3, 4, 5]
-2. [6, 8, 10]
-3. [5, 12, 13]
-
-The following function shows one way to solve the problem but the code is not ideal or efficient.
-Refactor the code based on what you have learned about code simplicity and efficiency.
-"""
-
 def my_function(Hypotenuse):
-    solutions = []
-    for hypo in range(5, Hypotenuse):
-        for adjacent in range(4, Hypotenuse):
-            for opposite in range(3, Hypotenuse):
-                if (hypo*hypo==adjacent*adjacent+opposite*opposite):
-                    solutions.append([hypo, adjacent, opposite])
+    solutions = [[hypo, adjacent, opposite] for hypo in range(5, Hypotenuse) for adjacent in range(4, Hypotenuse) for opposite in range(3, Hypotenuse) if (hypo*hypo==adjacent*adjacent+opposite*opposite)]
     maximum = 0
     for solution in solutions:
-        if maximum < max(solution):
-            maximum = max(solution)
+        maximum = max(solution)
     return maximum
 
 Hypotenuse = input("What is the maximal length of the triangle side? Enter a number: ")
